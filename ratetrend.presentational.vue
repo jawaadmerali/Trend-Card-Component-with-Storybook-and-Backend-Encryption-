88e200c45dd0ffc1fe954224f5c609f114c4cdc5
jawaadmerali <113218507+jawaadmerali@users.noreply.github.com>
2023-08-11 19:13:01 -0500

    Create ratetrend.presentational.vue

---
+++ ratetrend.presentational.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="ratetrend-presentational bg-default inline corner">
+        <a @click="onClick">
+            <div>
+                <i v-if="iconColor && iconColor.length" :class="`dot small bg-${iconColor}`"></i>
+                <b class="f7">{{ header }}</b>
+            </div>
+            <div>
+                <h3 class="text-bold f3">{{ description }}</h3>
+            </div>
+            <div v-if="footer" :class="`f7`"> {{ footer }}</div>
+        </a>
+    </div>
+ </template>
+
+<script>
+export default {
+    name: 'TrendItem',
+    props: {
+        iconColor: String,
+        header: String,
+        description: String,
+        footer: String,
+        onClick: Function,
+    },
+};
+</script>
